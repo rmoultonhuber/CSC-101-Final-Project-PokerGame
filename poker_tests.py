f23e34fa80a201850e3d058247cd3acdf78cb16e
Ruben Moulton Huber <rmoulton@calpoly.edu>
2025-03-03 17:23:44 -0800

    Formatting readme

--- poker_tests.py
+++ poker_tests.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
 
+from poker_code import shuffled_deck
+
 
 class TestPoker(TestCase):
 
     def test_deal_hand(self):
         length = 5
+        test_hand = shuffled_deck[:5]
+        self.assertEqual(len(test_hand),length)
+
+
+    def
+
