494f59543b47a560196f2a064c0d00cd78130c45
Ruben Moulton Huber <rmoulton@calpoly.edu>
2025-03-03 17:30:22 -0800

    Writing Tests

--- poker_tests.py
+++ poker_tests.py
@@ -5,11 +5,19 @@ from poker_code import shuffled_deck
 
 class TestPoker(TestCase):
 
-    def test_deal_hand(self):
+    def test_deal_hand1(self):
         length = 5
         test_hand = shuffled_deck[:5]
         self.assertEqual(len(test_hand),length)
 
+    def test_deal_hand2(self):
+        player_hand = shuffled_deck[:5]
+        dealer_hand = shuffled_deck[5:10]
+        self.assertEqual(len(player_hand),len(dealer_hand))
 
-    def
+    def test_get_hand_value1(self):
+        inpt = ["5 of hearts"]
+
+        self.assertEqual()
+
 
