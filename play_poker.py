828c843f2793c4b587439a16620b117a38d4188b
Jlopanec1 <jlopanec@calpoy.edu>
2025-03-03 17:40:20 -0800

    1st

---
+++ play_poker.py
@@ -0,0 +1,6 @@
+print("--------------------------WELCOME TO POKER--------------------------")
+print("      \n\n\n                        Enter 'Start' to play")
+
+print("\n \n \n--------------------------WELCOME TO POKER--------------------------")
+bet = 500
+if input() == "Start":
