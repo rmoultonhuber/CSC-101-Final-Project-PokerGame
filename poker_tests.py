cd070c943445285def37b8bfa4fbad098686eea0
Ruben Moulton Huber <rmoulton@calpoly.edu>
2025-03-05 14:45:44 -0800

    Test Cases

--- poker_tests.py
+++ poker_tests.py
@@ -1,8 +1,12 @@
+from os.path import expanduser
 from unittest import TestCase
 
-from poker_code import shuffled_deck
+import data
+from data import shuffle
+from poker_code import your_hand, dealer_hand
 
-
+shuffled_deck = shuffle(data.deck)
+print(shuffled_deck[:5])
 class TestPoker(TestCase):
 
     def test_deal_hand1(self):
@@ -16,8 +20,14 @@ class TestPoker(TestCase):
         self.assertEqual(len(player_hand),len(dealer_hand))
 
     def test_get_hand_value1(self):
-        inpt = ["5 of hearts"]
+        inpt = [data.Card('Clubs', '2'), data.Card('Clubs', '6'), data.Card('Spades', '2'), data.Card('Hearts', 'Jack'),data.Card('Diamonds', '4')]
+        expected = "Your Hand is: Pair"
+        result = your_hand(inpt)
+        self.assertEqual(expected,result)
 
-        self.assertEqual()
-
+    def test_get_dealer_hand_value1(self):
+        inpt = [data.Card('Clubs', '2'), data.Card('Clubs', '6'), data.Card('Spades', '2'), data.Card('Hearts', 'Jack'),data.Card('Diamonds', '4')]
+        expected = "Dealer Hand is: Pair"
+        result = dealer_hand(inpt)
+        self.assertEqual(expected,result)
 
